7a93ff13c24ca89830f707fc874e701055d99309
tecnarf <francgomez.tec@gmail.com>
2025-01-18 19:43:14 -0300

    Add files via upload

---
+++ esqueleto_programa_arduino.ino
@@ -0,0 +1,21 @@
+/*El lenguaje de programación utilizado para Arduino es una variante de C++, 
+aunque también incluye algunas simplificaciones y funciones específicas 
+diseñadas para facilitar el uso en la programación de microcontroladores.*/
+
+//Declaracion de variables globlales(si es necesario)
+//Son variables que se utilizaran en todo el programa
+//Se declaran fuera de cualquier función y, por lo tanto, 
+//son accesibles desde cualquier parte del programa, 
+//tanto en la función setup() como en la función loop() 
+//y en cualquier otra función que definas.
+
+void setup(){
+	//Trata de la configuracion inicial
+	//Se ejecuta una sola vez al inicio
+	//Aquí se configuran las entradas y salidas, así como cualquier otra inicialización
+}
+
+void loop(){
+	//La funcion se ejecuta en un bucle continuo.
+	//Aquí colocas el código que deseas que se ejecute repetidamente.
+}
